6396175acdbf58563c4e36a8aa0cfed99084605c
Thiago Mendes <tm.thiagom86@gmail.com>
2019-07-29 18:19:16 -0300

    Widgets e Sidebars

--- wp-content/themes/primeirotema/include/setup.php
+++ wp-content/themes/primeirotema/include/setup.php
@@ -10,4 +10,16 @@
 
  		register_nav_menu('primary', __('Primary Menu', 'primeirotema'));
  	}
+
+ 	function bp_widgets(){
+ 		register_sidebar(array(
+ 			'name' => __('Meu Primeiro Sidebar', 'primeirotema'),
+ 			'id' => 'bp_sidebar', 
+ 			'description' => __('Sidebar para o tema', 'primeirotema'),
+ 			'before_title' => '<h4 class="widget_title">',
+ 			'after_title' => '</h4>',
+ 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+ 			'after_widget' => '</div>'
+ 		));
+ 	}
  ?>
