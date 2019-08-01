df8716237500d5b15d12f384fe129507b38c2f77
Thiago Mendes <tm.thiagom86@gmail.com>
2019-08-01 12:15:06 -0300

    Listando posts, titulo, nome do autor, data do post, resumo do post, numero de comentarios, css para os posts

--- wp-content/themes/primeirotema/include/setup.php
+++ wp-content/themes/primeirotema/include/setup.php
@@ -8,6 +8,8 @@
  	function bp_after_setup(){
  		add_theme_support('menus');
 
+ 		add_theme_support('post-thumbnails');
+
  		register_nav_menu('primary', __('Primary Menu', 'primeirotema'));
  	}
 
