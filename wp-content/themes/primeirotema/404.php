f92b6979ae8006801d3725fa099e8424f9fe49a8
Thiago Mendes <tm.thiagom86@gmail.com>
2019-08-02 12:22:19 -0300

    Pagina unica e pagina nao encontrada

---
+++ wp-content/themes/primeirotema/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+	
+	<section>
+		
+		<div class="container">
+				<h1>Página não encontrada!</h1>
+
+				<p>A página que você está tentando acessar não foi encontrada.</p>
+			</div>
+		</div>
+
+		<div style="clear: both"></div>
+
+	</section>
+
+<?php get_footer(); ?>
