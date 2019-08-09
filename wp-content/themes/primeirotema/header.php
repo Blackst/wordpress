6cab0896f6ffccc95106d69046a651117ae98b44
Thiago Mendes <tm.thiagom86@gmail.com>
2019-08-09 16:15:58 -0300

    header, title e logo personalizada

--- wp-content/themes/primeirotema/header.php
+++ wp-content/themes/primeirotema/header.php
@@ -5,8 +5,15 @@
 </head>
 <body <?php body_class(); ?>>  <!--Posso colocar as minhas classes dentro de body_class-->
 	<header>
-		<h1>Meu Primeiro Tema</h1>
 
+		<?php 
+			if (has_custom_logo()) {
+				the_custom_logo();
+			}
+		 ?>
+		<h1><a href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a></h1>
+
+		<div style="clear: both;"></div>
 	<?php 
 		if (has_nav_menu('primary')) {
 			wp_nav_menu(array('theme_location' => 'primary',
--- wp-content/themes/primeirotema/include/setup.php
+++ wp-content/themes/primeirotema/include/setup.php
@@ -10,6 +10,10 @@
 
  		add_theme_support('post-thumbnails');
 
+ 		add_theme_support('title-tag');
+
+ 		add_theme_support('custom-logo');
+
  		register_nav_menu('primary', __('Primary Menu', 'primeirotema'));
  	}
 
