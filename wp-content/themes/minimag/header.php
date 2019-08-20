63cca1c03c6e64cf0c94d192d51e0017822d85f3
Thiago Mendes <tm.thiagom86@gmail.com>
2019-08-20 10:35:46 -0300

    Cabecalho parte 3

--- wp-content/themes/minimag/header.php
+++ wp-content/themes/minimag/header.php
@@ -7,7 +7,7 @@
 	<header>
 		<div class="top_header">
 			<nav class="navbar navbar-default">
-				<div class="container-fluid">
+				<div class="container">
 					<div class="collapse navbar-collapse">
 						<?php 
 							if (has_nav_menu('top')) {
@@ -25,7 +25,7 @@
 		</div>
 
 		<div class="main-header">
-			<div class="container-fluid">
+			<div class="container">
 				<div class="logo">
 					<?php if (has_custom_logo()) {
 						the_custom_logo();
@@ -51,11 +51,37 @@
 					</div>
 					<div class="main_info">
 						<div class="row">
-							<div class="col-sm-8">
-								...
+							<div class="col-sm-8 randompost">
+								<strong>Você já viu ?</strong>
+								<?php 
+								$tm_query = new WP_Query(array(
+									'posts_per_page' => 1,
+									'post_type' => 'post',
+									'orderby' => 'rand'
+								));
+								if ($tm_query->have_posts()) {
+									while ($tm_query->have_posts()) {
+										$tm_query->the_post();
+										?>
+										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+										<?php
+									}
+
+									wp_reset_postdata();
+								}
+								?>
 							</div>
 							<div class="col-sm-4 socialarea">
-								...
+								<div class="socialtxt">
+									SIGA:
+								</div>
+								<div class="socialicons">
+									<a href="https://www.facebook.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png"></a>
+									<a href="https://www.instagram.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.png"></a>
+									<a href="https://www.google.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gplus.png"></a>
+									<a href="https://www.twitter.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.png"></a>
+									<a href="https://www.youtube.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.png"></a>
+								</div>
 							</div>
 						</div>
 					</div>
